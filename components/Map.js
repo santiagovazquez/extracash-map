3bfacf8818b7ca8080d907b8faa36040bc0ba75b
Santiago Vazquez <santiagovazq@gmail.com>
2020-04-12 13:22:11 -0300

    Adding tags to website & geolocalization

--- components/Map.js
+++ components/Map.js
@@ -2,20 +2,42 @@ import { Component } from 'react';
 import MapaInteractivo from '@usig-gcba/mapa-interactivo/lib/MapaInteractivo';
 import { places } from '../files/places';
 
+const getPosition = () => {
+  return new Promise(function(resolve) {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        ({ coords }) => resolve({ latitude: coords.latitude.toFixed(2), longitude: coords.longitude.toFixed(2) }),
+        (e) => {
+          console.error(e);
+          resolve({ latitude: -34.62, longitude: -58.44 });
+        },
+      );
+    } else {
+      console.error('browser does not support geolocalization');
+      resolve({ latitude: -34.62, longitude: -58.44 });
+    }
+  });
+};
+
 export default class Map extends Component {
   componentDidMount() {
-    const mapaInteractivo = new MapaInteractivo("mapa", {
-      center: [-34.62, -58.44],
-      zoomControl: true,
-      attributionControl: true,
-      onMarkerClick: function(markerId) {
-        const p = places[markerId];
-        mapaInteractivo.addPopup([p.lat, p.lng], `<div><b>${p.name}</b></div><div>${p.street} ${p.number || ''}</div>`);
-      },
-    });
-    places.forEach((p, idx) => {
-      mapaInteractivo.addMarker({ lat: p.lat, lng: p.lng }, true, false, true, false, true, { markerId: idx });
-    });
+
+    getPosition()
+      .then((coords) => {
+        console.log(coords);
+        const mapaInteractivo = new MapaInteractivo("mapa", {
+          center: [coords.latitude, coords.longitude],
+          zoomControl: true,
+          attributionControl: true,
+          onMarkerClick: function(markerId) {
+            const p = places[markerId];
+            mapaInteractivo.addPopup([p.lat, p.lng], `<div><b>${p.name}</b></div><div>${p.street} ${p.number || ''}</div>`);
+          },
+        });
+        places.forEach((p, idx) => {
+          mapaInteractivo.addMarker({ lat: p.lat, lng: p.lng }, true, false, true, false, true, { markerId: idx });
+        });
+      })
   }
 
   render() {
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(
@@ -8,7 +10,37 @@ const Map = dynamic(
       </div>
     ), }
 );
+const pageTitle = 'Mapa con puntos Extra Cash (Visa) en CABA';
+const pageDescription = 'Encontrá el punto extra cash más cerca de tu domicilio y evitá ir al cajero';
+const keywords = 'extra,cash,efectivo,red,cajero,mapa,puntos,visa,debito,tarjeta,retiro,dinero,plata,evitar,banco';
+const webURL = 'https://extracash-map.svazquez.now.sh';
 
 export default function Index() {
-   return <Map/>;
+   return (
+     <>
+       <Head>
+         <meta name="google-site-verification" content="ABgOjF6kiEqelusHuGukbWql419PGXFE_zikhe_9Vy4" />
+         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+         <meta httpEquiv="Content-Language" content="es" />
+         <title>{pageTitle}</title>
+         <meta name="title" content={pageTitle} />
+         <meta httpEquiv="title" content={pageTitle} />
+         <meta name="DC.title" content={pageTitle} />
+         <meta name="description" lang="es" content={pageDescription} />
+         <meta httpEquiv="DC.Description" content={pageDescription} />
+         <meta httpEquiv="DC.subject" content={pageDescription} />
+         {/*5 o 6 keywords importantes*/}
+         <meta name="keywords" content={keywords} />
+         <meta httpEquiv="DC.Keywords" content={keywords} />
+
+         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+         <meta property="og:title" content={pageTitle} />
+         <meta property="og:description" content={pageDescription} />
+         <meta property="og:image" content="/public/assets/preview.png" />
+         <meta property="og:type" content="website" />
+         <meta property="og:url" content={webURL} />
+       </Head>
+       <Map/>
+     </>
+   );
 };
